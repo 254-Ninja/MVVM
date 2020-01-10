aad0370047244576e6476ac11e7da2703439a2b1
Denis Muthomi <muthomidenis62@gmail.com>
2020-01-10 13:46:44 +0300

    Data Binding

--- app/src/main/java/com/example/mvvmusingdatabinding/MainActivity.java
+++ app/src/main/java/com/example/mvvmusingdatabinding/MainActivity.java
@@ -11,4 +11,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public class User {
+        private String email;
+        private String password;
+
+        public User(String email, String password) {
+            this.email = email;
+            this.password = password;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+
+        public String getPassword() {
+            return password;
+        }
+
+
+    }
 }
