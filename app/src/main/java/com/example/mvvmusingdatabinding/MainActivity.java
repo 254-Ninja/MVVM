0e6dcb61a60109759e540b9d861ac9b67a7421f6
Denis Muthomi <muthomidenis62@gmail.com>
2020-01-10 14:46:40 +0300

    UpDate

--- app/src/main/java/com/example/mvvmusingdatabinding/MainActivity.java
+++ app/src/main/java/com/example/mvvmusingdatabinding/MainActivity.java
@@ -1,44 +1,33 @@
 package com.example.mvvmusingdatabinding;
 
-import androidx.appcompat.app.AppCompatActivity;
+
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.journaldev.androidmvvmbasics.R;
+import com.journaldev.androidmvvmbasics.databinding.ActivityMainBinding;
+import com.journaldev.androidmvvmbasics.viewmodels.LoginViewModel;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        activityMainBinding.setViewModel(new LoginViewModel());
+        activityMainBinding.executePendingBindings();
+
     }
 
-    public class User {
-        private String email;
-        private String password;
-
-        public User(String email, String password) {
-            this.email = email;
-            this.password = password;
-        }
-
-        public void setEmail(String email) {
-            this.email = email;
-        }
-
-
-        public String getEmail() {
-            return email;
-        }
-
-        public void setPassword(String password) {
-            this.password = password;
-        }
-
-
-        public String getPassword() {
-            return password;
-        }
-
-
+    @BindingAdapter({"toastMessage"})
+    public static void runMe(View view, String message) {
+        if (message != null)
+            Toast.makeText(view.getContext(), message, Toast.LENGTH_SHORT).show();
     }
 }
